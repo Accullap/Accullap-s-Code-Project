a4cd8505cd0620781aa830c2afd38693e3835e39
PakosPablos <danielmatospedroso@outlook.com>
2026-03-07 14:32:07 +0100

    changes

---
+++ Inventory Management/Item.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self,name,price,quantaty,IdNum,Fourniseur):
+        self.name = name
+        self.price = price
+        self.quantaty = quantaty
+        self.IdNum = IdNum
+        self.Fourniseur = Fourniseur
+
+    
