f9b0aa2a456fb075d2b8fa1d25c594ccfb75ff0a
Accullap <palluccal4@gmail.com>
2026-04-06 23:33:28 +0200

    check diagonal

--- connect4.py
+++ connect4.py
@@ -12,24 +12,25 @@ def printBoard():
             print("---+---+---+---+---+---+---")
 
 
-# def playerInput():
-#     while True:
-#         print()
-#         if currentplayer == " X ":
-#             inp = int(input(f"Enter a number 1-9 Player (X): "))
-#         else:
-#             inp = int(input(f"Enter a number 1-9 Player (O): "))
-#         print()
-#         if inp >= 1 and inp <= 9 and board[inp-1] == "   ":
-#             board[inp-1] = currentplayer
-#             break
-#         else:
-#             print("Oops! Try again!\n")
-#             printBoard()
+def playerInput():
+    #     while True:
+    #         print()
+    #         if currentplayer == " X ":
+    #             inp = int(input(f"Enter a number 1-9 Player (X): "))
+    #         else:
+    #             inp = int(input(f"Enter a number 1-9 Player (O): "))
+    #         print()
+    #         if inp >= 1 and inp <= 9 and board[inp-1] == "   ":
+    #             board[inp-1] = currentplayer
+    #             break
+    #         else:
+    #             print("Oops! Try again!\n")
+    printBoard()
+
 
 def checkHorizontal():
     global winner
-    # Check column 1 - 6 every 4 lines is every pssibility to win for first column.
+    # Check column 1 - 6 every 4 lines is every p0ssibility to win for first column.
     if (board[0] == board[1] == board[2] == board[3] and board[0] != "   ") or \
         (board[1] == board[2] == board[3] == board[4] and board[1] != "   ") or \
         (board[2] == board[3] == board[4] == board[5] and board[2] != "   ") or \
@@ -60,6 +61,7 @@ def checkHorizontal():
 
 def checkRow():
     global winner
+    # Check row 1 - 7 every 3 lines is every possibility to win for first row.
     if (board[0] == board[7] == board[14] == board[21] and board[0] != "   ") or \
         (board[7] == board[14] == board[21] == board[28] and board[7] != "   ") or \
         (board[14] == board[21] == board[28] == board[35] and board[14] != "   ") or \
@@ -87,34 +89,62 @@ def checkRow():
 
 def checkDiagonal():
     global winner
-    if (board[0] == board[4] == board[8] and board[0] != "   ") or \
-       (board[2] == board[4] == board[6] and board[2] != "   "):
+    # Check diagonal first 12 lines -> top left to bottem right then last 12 lines -> top right to bottom left
+    if (board[0] == board[8] == board[16] == board[24] and board[0] != "   ") or \
+        (board[1] == board[9] == board[17] == board[25] and board[1] != "   ") or \
+        (board[2] == board[10] == board[18] == board[26] and board[2] != "   ") or \
+        (board[3] == board[11] == board[19] == board[27] and board[3] != "   ") or \
+        (board[7] == board[15] == board[23] == board[31] and board[7] != "   ") or \
+        (board[8] == board[16] == board[24] == board[32] and board[8] != "   ") or \
+        (board[9] == board[17] == board[25] == board[33] and board[9] != "   ") or \
+        (board[10] == board[18] == board[26] == board[34] and board[10] != "   ") or \
+        (board[14] == board[22] == board[30] == board[38] and board[14] != "   ") or \
+        (board[15] == board[23] == board[31] == board[39] and board[15] != "   ") or \
+        (board[16] == board[24] == board[32] == board[40] and board[16] != "   ") or \
+        (board[17] == board[25] == board[33] == board[41] and board[17] != "   ") or \
+        (board[3] == board[9] == board[15] == board[21] and board[3] != "   ") or \
+        (board[4] == board[10] == board[16] == board[22] and board[4] != "   ") or \
+        (board[5] == board[11] == board[17] == board[23] and board[5] != "   ") or \
+        (board[6] == board[12] == board[18] == board[24] and board[6] != "   ") or \
+        (board[10] == board[16] == board[22] == board[28] and board[10] != "   ") or \
+        (board[11] == board[17] == board[23] == board[29] and board[11] != "   ") or \
+        (board[12] == board[18] == board[24] == board[30] and board[12] != "   ") or \
+        (board[13] == board[19] == board[25] == board[31] and board[13] != "   ") or \
+        (board[17] == board[23] == board[29] == board[35] and board[17] != "   ") or \
+        (board[18] == board[24] == board[30] == board[36] and board[18] != "   ") or \
+        (board[19] == board[25] == board[31] == board[37] and board[19] != "   ") or \
+            (board[20] == board[26] == board[32] == board[38] and board[20] != "   "):
         winner = currentplayer
         return True
 
-# def checkTie():
-#     global gamerunning
-#     if "   " not in board:
-#         printBoard()
-#         print("Its a tie")
-#         gamerunning = False
 
-# def checkWin():
-#     if checkDiagonal() or checkHorizontal() or checkRow():
-#         print(f"The winner is {winner}\n")
+# still need to add if no possibility to win but "   " still in board -> gamerunning = False
+def checkTie():
+    global gamerunning
+    if "   " not in board:
+        printBoard()
+        print("Its a tie")
+        gamerunning = False
 
-# def switchPlayer():
-#     global currentplayer
-#     if currentplayer == " X ":
-#         currentplayer = " O "
-#     else:
-#         currentplayer = " X "
 
-# while gamerunning:
-#     printBoard()
-#     if winner != None:
-#         break
-#     playerInput()
-#     checkWin()
-#     checkTie()
-#     switchPlayer()
+def checkWin():
+    if checkDiagonal() or checkHorizontal() or checkRow():
+        print(f"The winner is {winner}\n")
+
+
+def switchPlayer():
+    global currentplayer
+    if currentplayer == " X ":
+        currentplayer = " O "
+    else:
+        currentplayer = " X "
+
+
+while gamerunning:
+    printBoard()
+    if winner != None:
+        break
+    playerInput()
+    checkWin()
+    checkTie()
+    switchPlayer()
