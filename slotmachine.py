fb753fe3e4297c9f994de09b4ee862ff96e0060d
Accullap <palluccal4@gmail.com>
2026-04-08 12:59:37 +0200

    slot symbol count

---
+++ slotmachine.py
@@ -0,0 +1,28 @@
+import random
+
+max_lines = 3
+min_lines = 1
+max_bet = 100
+
+rows = 3
+cols = 6
+
+symbol_count = {
+    "A": 3,
+    "B": 6,
+    "C": 9,
+    "D": 12
+}
+
+symbol_value = {
+    "A": 16,
+    "B": 8,
+    "C": 4,
+    "D": 2,
+}
+
+
+def value_slots():
+
+
+def display():
