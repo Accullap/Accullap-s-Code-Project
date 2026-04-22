abbc9ef56654148d7d70054eb99b9adcc74a96e2
Lenny Pallucca <palluccal4@gmail.com>
2026-04-22 10:51:05 +0200

    added / and -

--- ufunc.py
+++ ufunc.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-def add(x, y):
+def add(x,y):
     return x+y
 
 np.add = np.frompyfunc(add, 2, 1)
@@ -8,13 +8,23 @@ np.add = np.frompyfunc(add, 2, 1)
 k = np.add([3, 4, 5],[1, 2, 3])
 
 
-def multiply(x, y):
+def multiply(x,y):
     return x*y 
 
 np.multiply = np.frompyfunc(multiply, 2, 1)
 
-j = np.array([1,2,3])
+j = np.array([7,8,9])
 p = np.array([4,5,6])
 
-print(np.multiply(j, p))
+def div(x,y):
+    return x/y
 
+np.div = np.frompyfunc(div, 2, 1)
+
+
+def subtract(x,y):
+    return x-y
+
+np.subtract = np.frompyfunc(subtract, 2, 1)
+
+print(np.subtract(j,p))
